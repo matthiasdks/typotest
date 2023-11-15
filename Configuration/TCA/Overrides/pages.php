f394571c7d4c55c57143138543acfa9efe7e269b
Matthias <Matthias.duemmel@group-work.de>
2023-11-15 13:58:29 +0100

    Add skeleton folders

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,18 @@
+<?php
+defined('TYPO3') or die('Access denied.');
+call_user_func(function()
+{
+    /**
+     * Temporary variables
+     */
+    $extensionKey = 'typotest';
+
+    /**
+     * Default PageTS for Typotest
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        $extensionKey,
+        'Configuration/TsConfig/Page/All.tsconfig',
+        'TypoTest'
+    );
+});
---
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * TypoTest v1.0.0 (https://group-work.de)
+ * Copyright 2017-2023 Matthias Dümmel
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
